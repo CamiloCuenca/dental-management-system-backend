92dd6cf3a1d93a4199017538a5cd47d0d2200e68
CamiloCuenca <camilocuenca1810@gmail.com>
2025-02-21 14:49:44 -0500

    Initial commit

---
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/DentalManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uniquindio.dentalmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/uniquindio/dentalmanagementsystembackend/DentalManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uniquindio.dentalmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
