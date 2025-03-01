b749392b27c2d1df8b0ba76e82df5d9cd3c273c4
CamiloCuenca <camilocuenca1810@gmail.com>
2025-03-01 00:33:51 -0500

    feat(exception): agregar GlobalExceptionHandler para manejar excepciones en las peticiones #4

---
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package edu.uniquindio.dentalmanagementsystembackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.internalServerError().body("Ocurrió un error inesperado.");
+    }
+}
