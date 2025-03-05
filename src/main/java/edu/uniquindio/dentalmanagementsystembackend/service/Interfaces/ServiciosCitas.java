e8dd863c6d7fdd7cc5b089c1488663774934b16b
CamiloCuenca <camilocuenca1810@gmail.com>
2025-03-05 13:55:39 -0500

    feat(service): agregar método para listar todas las citas, cancelar un cita y editar un cita

--- src/main/java/edu/uniquindio/dentalmanagementsystembackend/service/Interfaces/ServiciosCitas.java
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/service/Interfaces/ServiciosCitas.java
@@ -1,5 +1,6 @@
 package edu.uniquindio.dentalmanagementsystembackend.service.Interfaces;
 
+import edu.uniquindio.dentalmanagementsystembackend.Enum.TipoCita;
 import edu.uniquindio.dentalmanagementsystembackend.dto.CitaDTO;
 
 import java.util.ArrayList;
@@ -7,9 +8,38 @@ import java.util.List;
 
 public interface ServiciosCitas {
 
-    // Método para crear (agendar) una cita odontológica
+
+    /**
+     * Método para crear (agendar) una cita odontológica
+     * @param citaDTO
+     */
     void crearCita(CitaDTO citaDTO);
 
-    // Método para obtener la lista de citas de un paciente
+
+
+    /**
+     * Método para obtener la lista de citas de un paciente
+     * @param idPaciente
+     * @return
+     */
     List<CitaDTO> obtenerCitasPorPaciente(Long idPaciente);
+
+    /**
+     *
+     * @return
+     */
+    List<CitaDTO> obtenerTodasLasCitas(); // Obtener todas las citas
+
+    /**
+     *
+     * @param idCita
+     * @param nuevoTipoCita
+     */
+    void editarCita(Long idCita, TipoCita nuevoTipoCita); // Editar tipo de cita
+
+    /**
+     *
+     * @param idCita
+     */
+    void cancelarCita(Long idCita); // Cancelar cita
 }
