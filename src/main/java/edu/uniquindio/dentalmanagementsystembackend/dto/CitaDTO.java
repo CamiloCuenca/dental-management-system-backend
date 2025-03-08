c9ef6bfb7c34704cd3ba9db9c8bd0a335001d1ac
CamiloCuenca <camilocuenca1810@gmail.com>
2025-03-08 15:52:26 -0500

    update: modificar el servicio de creación de citas para considerar la disponibilidad de horarios #4

--- src/main/java/edu/uniquindio/dentalmanagementsystembackend/dto/CitaDTO.java
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/dto/CitaDTO.java
@@ -3,12 +3,8 @@ package edu.uniquindio.dentalmanagementsystembackend.dto;
 import edu.uniquindio.dentalmanagementsystembackend.Enum.EstadoCitas;
 import edu.uniquindio.dentalmanagementsystembackend.Enum.TipoCita;
 
-import java.time.LocalDateTime;
-
 public record CitaDTO(
         Long idPaciente,
-        Long idDoctor,
-        LocalDateTime fechaHora,
         EstadoCitas estado,
         TipoCita tipoCita
 ) {
