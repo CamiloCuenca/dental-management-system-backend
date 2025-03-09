fd420fddb746e5fba7089970d48c50884a6bb4b0
CamiloCuenca <camilocuenca1810@gmail.com>
2025-03-09 15:36:47 -0500

    add: configuracion y servicio para tratar la verificacion del reCAPTCHA

---
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/service/impl/CaptchaService.java
@@ -0,0 +1,48 @@
+package edu.uniquindio.dentalmanagementsystembackend.service.impl;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CaptchaService {
+
+    @Value("${recaptcha.secret-key}")
+    private String recaptchaSecretKey;
+
+    private static final String GOOGLE_RECAPTCHA_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+
+    private final WebClient webClient;
+
+    public CaptchaService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl(GOOGLE_RECAPTCHA_VERIFY_URL)
+                .build();
+    }
+
+    public Mono<Boolean> verifyCaptcha(String captchaToken) {
+        return webClient.post()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("secret", recaptchaSecretKey)
+                        .queryParam("response", captchaToken)
+                        .build())
+                .retrieve()
+                .bodyToMono(GoogleResponse.class)
+                .map(GoogleResponse::isSuccess)
+                .onErrorReturn(false); // Manejo de errores: si falla, se devuelve false
+    }
+
+    @Data
+    private static class GoogleResponse {
+        private boolean success;
+
+        @JsonProperty("challenge_ts")
+        private String challengeTs;
+
+        private String hostname;
+        private float score;
+    }
+}
