cd231bdfd63b19d29091d5d250e0cdeea3c1f54c
CamiloCuenca <camilocuenca1810@gmail.com>
2025-05-12 15:46:05 -0500

    feat(enum): add EstadoInventario and TipoProducto enums for inventory management

---
+++ src/main/java/edu/uniquindio/dentalmanagementsystembackend/Enum/EstadoInventario.java
@@ -0,0 +1,10 @@
+package edu.uniquindio.dentalmanagementsystembackend.Enum;
+
+public enum EstadoInventario {
+    DISPONIBLE,
+    AGOTADO,
+    RESERVADO,
+    VENCIDO,
+    DAÑADO,
+    EN_REPOSICION
+}
